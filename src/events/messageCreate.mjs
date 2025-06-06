315a02db268b792985cd30c30d0aed511a05be94
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2025-06-05 23:44:39 -0400

    Update messageCreate.mjs

--- src/events/messageCreate.mjs
+++ src/events/messageCreate.mjs
@@ -3,9 +3,12 @@ import { getMsg } from '../locales.mjs';
 
 // Event handler for messageCreate
 export default async function (message) {
-    log.debug('messageCreate', { message });
-    if (message.author.bot) return;
-    if (message.content === '!ping') {
-        message.reply('Pong!');
+    if (!message.guild) message.reply('Direct messages are not supported. Please use a channel in a server.');
+    if (!message.mentions.has(message.client.user) && !message.reference?.message?.author?.id === message.client.user.id) {
+        return;
     }
+    if (!message.member.permissions.has('ADMINISTRATOR')) {
+        return;
+    }
+    message.reply("Hello");
 }
