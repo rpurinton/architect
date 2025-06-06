edb5f11e5797c3757ef0c4a987dd294b681b5a1b
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2025-06-06 00:14:47 -0400

    Update messageCreate.mjs

--- src/events/messageCreate.mjs
+++ src/events/messageCreate.mjs
@@ -8,6 +8,7 @@ export default async function (message) {
     if (!message.guild) message.reply('Direct messages are not supported. Please use a channel in a server.');
     if (!message.mentions.has(message.client.user) && !message.reference?.message?.author?.id !== message.client.user.id) return;
     if (!message.member.permissions.has('ADMINISTRATOR')) return;
+    message.channel.sendTyping();
     const messages = await message.channel.messages.fetch({ limit: 100 });
     const reply = await getReply(message.client.user.id, messages);
     if (!reply) {
